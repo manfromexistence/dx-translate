4c0c5dd69a001cb6ff4b6b31a2baaeedbaa52acc
robinjusticeleague <robinjusticeleague@gmail.com>
2025-06-24 11:44:57 +0000

    feat: cli is working

--- src/cli.ts
+++ src/cli.ts
@@ -6,21 +6,22 @@ import cfonts from "cfonts";
 
 async function main() {
   console.clear();
-  
-  // cfonts.say will print the title directly to the console.
-  cfonts.say('Dx Translate', {
-    font: 'block',              // define the font face
-    align: 'left',              // define text alignment
-    background: 'transparent',  // define the background color
-    letterSpacing: 1,           // define letter spacing
-    lineHeight: 1,              // define the line height
-    space: true,                // define if the output text should have empty lines on top and on the bottom
-    maxLength: '0',             // define how many character can be on one line
-    gradient: ['cyan', 'blue'], // define your two gradient colors
-    independentGradient: true,  // define if you want to recalculate the gradient for each new line
-    transitionGradient: false,  // define if this is a transition between colors directly
-    env: 'node'                 // define the environment cfonts is being executed in
+
+  const title: any = cfonts.say('Dx Translate', {
+    font: 'block',
+    align: 'left',
+    background: 'transparent',
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+    maxLength: '0',
+    gradient: ['cyan', 'blue'],
+    independentGradient: true,
+    transitionGradient: false,
+    env: 'node'
   });
+
+  intro(title);
 
   const provider = await select({
     message: 'Which translation provider would you like to use?',
@@ -29,7 +30,7 @@ async function main() {
       { value: 'Google', label: 'Google' },
     ],
   });
-  
+
   if (isCancel(provider)) {
     outro('Operation cancelled.');
     return;
@@ -42,7 +43,7 @@ async function main() {
       if (!input) return 'Please enter some text.';
     },
   });
-  
+
   if (isCancel(textToTranslate)) {
     outro('Operation cancelled.');
     return;
@@ -51,12 +52,12 @@ async function main() {
   let sourceLang: string | symbol = 'auto';
   if (provider === 'MyMemory') {
     const myMemorySourceLang = await text({
-        message: 'Enter the source language (e.g., "en", "english"):',
-        initialValue: 'english'
+      message: 'Enter the source language (e.g., "en", "english"):',
+      initialValue: 'english'
     });
     if (isCancel(myMemorySourceLang)) {
-        outro('Operation cancelled.');
-        return;
+      outro('Operation cancelled.');
+      return;
     }
     sourceLang = myMemorySourceLang;
   }
@@ -71,7 +72,7 @@ async function main() {
     outro('Operation cancelled.');
     return;
   }
-  
+
   const s = spinner();
   s.start('Translating...');
 
