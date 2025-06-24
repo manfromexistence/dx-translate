bc0973d0bfa76293cf6e471870295ad0b806dd5b
robinjusticeleague <robinjusticeleague@gmail.com>
2025-06-24 11:35:48 +0000

    feat: cfonts

--- src/cli.ts
+++ src/cli.ts
@@ -2,10 +2,29 @@ import { intro, outro, select, text, confirm, spinner } from '@clack/prompts';
 import { GoogleTranslator } from '@/translators/google';
 import { MyMemoryTranslator } from '@/translators/mymemory';
 import { isCancel } from '@clack/prompts';
+import figlet from 'figlet';
+import cfonts from "cfonts";
 
 async function main() {
   console.clear();
-  intro(`TS-Translator CLI`);
+  // const title = figlet.textSync('Dx Translate');
+  const title:any = cfonts.say('Dx Translate', {
+    font: 'block',              // define the font face
+    align: 'left',              // define text alignment
+    colors: ['system'],         // define all colors
+    background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+    letterSpacing: 1,           // define letter spacing
+    lineHeight: 1,              // define the line height
+    space: true,                // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0',             // define how many character can be on one line
+    gradient: false,            // define your two gradient colors
+    independentGradient: false, // define if you want to recalculate the gradient for each new line
+    transitionGradient: false,  // define if this is a transition between colors directly
+    rawMode: false,             // define if the line breaks should be CRLF (`\r\n`) over the default LF (`\n`)
+    env: 'node'                 // define the environment cfonts is being executed in
+  });
+
+  intro(title);
 
   const provider = await select({
     message: 'Which translation provider would you like to use?',
@@ -68,7 +87,6 @@ async function main() {
       translatedText = await googleTranslator.translate(textToTranslate as string);
       await googleTranslator.close();
     } else {
-      // MyMemory will now always use the specified email for a higher limit
       const myMemoryTranslator = new MyMemoryTranslator({
         source: sourceLang as string,
         target: targetLang as string,
